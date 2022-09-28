bcf2509f216a1913c29eeeafbf339ed7cfdb1c64
chae <luxury515@naver.com>
2022-09-28 10:46:19 +0900

    log4j-api,log4j-core library

---
+++ src/main/java/StreamTest.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class StreamTest {
+    private static final Logger logger = LogManager.getLogger(StreamTest.class);
+
+    public static void main(String[] args) {
+        //
+        List<String> classList = Arrays.asList("java", "spring framework", "spring jpa", "java8");
+        long count = 0;
+
+        for (String className : classList) {
+            if (className.contains("java")) {
+                count++;
+            }
+        }
+        logger.debug("java 가 들어간 강좌 갯수 : {}" ,count);
+
+        long streamCount = classList.stream().filter(className -> className.contains("java"))
+            .count();
+
+        logger.debug("Stream 으로 java count :{}" , streamCount);
+
+        Stream<String> streamString = Arrays.stream(new String[]{"oh", "kim", "lee"});
+        logger.debug("stream String:{}",streamString.toList());
+    }
+
+}
